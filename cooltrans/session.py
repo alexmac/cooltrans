fe1bc4ed4d60a1b0c24ca290c87b606895fb04df
Alex Macdonald <alex@alexmac.cc>
2024-03-23 21:17:59 -0700

    XRay support

--- cooltrans/session.py
+++ cooltrans/session.py
@@ -4,13 +4,18 @@ import aiohttp
 
 from cooltrans.static_resolver import StaticResolver
 
+from aws_xray_sdk.ext.aiohttp.client import aws_xray_trace_config
+
 _shared_sessions_for_ip: Dict[str, aiohttp.ClientSession] = {}
 
 
 def get_shared_session_for_ip(ip: str) -> aiohttp.ClientSession:
     if ip not in _shared_sessions_for_ip:
         con = aiohttp.TCPConnector(resolver=StaticResolver(ip))
-        _shared_sessions_for_ip[ip] = aiohttp.ClientSession(connector=con)
+        tc = aws_xray_trace_config()
+        _shared_sessions_for_ip[ip] = aiohttp.ClientSession(
+            connector=con, trace_configs=[tc]
+        )
 
     return _shared_sessions_for_ip[ip]
 
@@ -21,6 +26,7 @@ _shared_session: Optional[aiohttp.ClientSession] = None
 def get_shared_session() -> aiohttp.ClientSession:
     global _shared_session
     if _shared_session is None:
-        _shared_session = aiohttp.ClientSession()
+        tc = aws_xray_trace_config()
+        _shared_session = aiohttp.ClientSession(trace_configs=[tc])
 
     return _shared_session
